c8e3e720d9f4efe8b09ea28a3195f4252d3a0157
Gregor Rammal <gregor.rammal@gmail.com>
2023-05-10 15:02:42 +0300

    nupud on klikitavad

--- src/main/java/com/example/male_javafx/Main.java
+++ src/main/java/com/example/male_javafx/Main.java
@@ -1,23 +1,16 @@
 package com.example.male_javafx;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 	@Override
@@ -49,109 +42,68 @@ public class Main extends Application {
 				grid.add(button, c, r);
 				button.setStyle("-fx-background-color:#CD7F32; -fx-border-color: #000000; -fx-border-width: 2px");
 
-
-				if (r == 1){
-					ImageView imageView = new ImageView(ettur_must);
-					imageView.setFitHeight(100);
-					imageView.setFitWidth(100);
-					button.setGraphic(imageView);
-				}
-
-				if (r == 6){
-					ImageView imageView = new ImageView(ettur_valge);
-					imageView.setFitHeight(100);
-					imageView.setFitWidth(100);
-					button.setGraphic(imageView);
-				}
-
-				if (c == 0 || c == 7){
-					if (r == 0){
-						ImageView imageView = new ImageView(vanker_must);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
+				button.setOnMouseClicked(event -> {
+					if (!Objects.equals(button.getGraphic().getId(), "0")) {
+						if (button.getStyle().equals("-fx-background-color:#CD7F32; -fx-border-color: #000000; -fx-border-width: 2px")) {
+							button.setStyle("-fx-background-color:#228B22; -fx-border-color: #000000; -fx-border-width: 2px");
+						} else {
+							button.setStyle("-fx-background-color:#CD7F32; -fx-border-color: #000000; -fx-border-width: 2px");
+						}
 					}
-					if (r == 7){
-						ImageView imageView = new ImageView(vanker_valge);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-				}
+				});
+
+				ImageView imageView = new ImageView();
+				imageView.setId("0");
+
+
+
+				if (r == 1)
+					imageView = new ImageView(ettur_must);
+				if (r == 6)
+					imageView = new ImageView(ettur_valge);
 
 
 				if (r == 0){
-					if (c == 0 || c == 7){
-						ImageView imageView = new ImageView(vanker_must);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-					if (c == 1 || c == 6){
-						ImageView imageView = new ImageView(ratsu_must);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-					if (c == 2 || c == 5){
-						ImageView imageView = new ImageView(oda_must);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-					if (c == 3){
-						ImageView imageView = new ImageView(lipp_must);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-					if (c == 4){
-						ImageView imageView = new ImageView(kuningas_must);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
+					if (c == 0 || c == 7)
+						imageView = new ImageView(vanker_must);
+					if (c == 1 || c == 6)
+						imageView = new ImageView(ratsu_must);
+					if (c == 2 || c == 5)
+						imageView = new ImageView(oda_must);
+					if (c == 3)
+						imageView = new ImageView(lipp_must);
+					if (c == 4)
+						imageView = new ImageView(kuningas_must);
 				}
 
 				if (r == 7){
-					if (c == 0 || c == 7){
-						ImageView imageView = new ImageView(vanker_valge);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-					if (c == 1 || c == 6){
-						ImageView imageView = new ImageView(ratsu_valge);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-					if (c == 2 || c == 5){
-						ImageView imageView = new ImageView(oda_valge);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-					if (c == 3){
-						ImageView imageView = new ImageView(lipp_valge);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
-					if (c == 4){
-						ImageView imageView = new ImageView(kuningas_valge);
-						imageView.setFitHeight(100);
-						imageView.setFitWidth(100);
-						button.setGraphic(imageView);
-					}
+					if (c == 0 || c == 7)
+						imageView = new ImageView(vanker_valge);
+					if (c == 1 || c == 6)
+						imageView = new ImageView(ratsu_valge);
+					if (c == 2 || c == 5)
+						imageView = new ImageView(oda_valge);
+					if (c == 3)
+						imageView = new ImageView(lipp_valge);
+					if (c == 4)
+						imageView = new ImageView(kuningas_valge);
 				}
 
+
+				imageView.setFitHeight(100);
+				imageView.setFitWidth(100);
+				button.setGraphic(imageView);
 
 			}
 		}
 
-		ScrollPane scrollPane = new ScrollPane(grid);
 
+
+
+
+
+
+		ScrollPane scrollPane = new ScrollPane(grid);
 		stage.setScene(new Scene(scrollPane));
 		stage.show();
 	}
